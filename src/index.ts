6593cf271a4fa2df79f9dbc7e60253690524fae3
Chris Ramón <rchristian.ramon@gmail.com>
2025-08-16 14:59:57 -0500

    src/index: adds cache service api

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,44 @@
 import { Service } from "./cache/service";
+import express from "express";
 
+const app: express.Application = express();
+
+// Enable JSON body parsing.
+app.use(express.json());
+
+const port: number = 3000;
 const service = new Service();
 
-service.add("key", "123");
-console.log(service.get("key"));
-service.remove("key");
-console.log(service.get("key"));
+// Handle POST requests in the '/post' path.
+app.post("/cache/add", (req: express.Request, res: express.Response) => {
+  const body = req.body;
+  const { key, value } = body;
+  service.add(key, value);
+  res.send();
+});
+
+// Handle GET requests in the '/get' path.
+app.get("/cache/get", (req: express.Request, res: express.Response) => {
+  const body = req.body;
+  const { key } = body;
+  const value = service.get(key);
+  res.send(value);
+});
+
+// Handle POST requests in the '/remove' path.
+app.post("/cache/remove", (req: express.Request, res: express.Response) => {
+  const body = req.body;
+  const { key } = body;
+  service.remove(key);
+  res.send();
+});
+
+// Handle GET requests in the '/ping' path.
+app.get("/ping", (req: express.Request, res: express.Response) => {
+  res.send("OK");
+});
+
+// Starts the server.
+app.listen(port, () => {
+  console.log(`Server listening at http://localhost:${port}`);
+});
