3994deaa9af675cdf32ec81c6b861627f14778b6
Chris Ramón <rchristian.ramon@gmail.com>
2025-08-16 14:24:41 -0500

    src: wires cache service

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,8 @@
-console.log("ok");
+import { Service } from "./cache/service";
+
+const service = new Service();
+
+service.add("key", "123");
+console.log(service.get("key"));
+service.remove("key");
+console.log(service.get("key"));
